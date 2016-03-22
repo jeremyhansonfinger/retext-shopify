4016b6878200b6d7a4519b1f8f5d9780bbf09636
Jeremy Hanson-Finger <jeremy.hansonfinger@shopify.com>
2016-03-22 19:10:44 -0400

    ports replacements from retext-styleguide, adds a couple test phrases

--- test.js
+++ test.js
@@ -5,14 +5,14 @@ var report = require('vfile-reporter');
 retext()
     .use(shopify)
     .process([
-        'I love using Liquid.',
+        'I love using Liquid once the.',
         'I love using liquid.',
         'I\'m on the Shopify unlimited Plan',
         'I\'m on the Shopify Unlimited plan',
         'I\'m on the Shopify UnLimited plan',
         'I love Shopify point of sale',
         'I love shopify point of sale',
-        'Unfortunately, I need to e.g. customise my drink',
+        'Unfortunately, I need to close down, i.e., customise my drink',
         'Oops, I need to Customise my drink with the shopify manual',
         '!'
     ].join('\n'), function (err, file) {
